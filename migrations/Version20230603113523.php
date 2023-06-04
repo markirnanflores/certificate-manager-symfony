373cc5e538e8b459cf310826fb1388b6c165171a
markirnanflores <info@markirnanflores.com>
2023-06-04 12:59:07 +0200

    Updated migration

--- migrations/Version20230603113523.php
+++ migrations/Version20230603113523.php
@@ -20,15 +20,19 @@ final class Version20230603113523 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE certificate_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE certificate (id INT NOT NULL, name VARCHAR(255) NOT NULL, certificate VARCHAR(255) NOT NULL, private_key VARCHAR(255) NOT NULL, intermediate_ca VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE `certificates` (
+            `id` INT NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(255) NOT NULL,
+            `certificate` VARCHAR(255) NOT NULL,
+            `private_key` VARCHAR(255) NOT NULL,
+            `intermediate_ca` VARCHAR(255)
+        );');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE certificate_id_seq CASCADE');
         $this->addSql('DROP TABLE certificate');
     }
 }
